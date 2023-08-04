8a963091e8fe2ea8850eaa87fa27c58bea3ce6bc
Mozibx <opuzmoses@gmail.com>
2023-08-04 14:08:39 +0100

    chores: updated navbar functionalities

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-const toogleBtn = document.querySelector('.toogle_btn');
-const toogleBtnIcon = document.querySelector('.toogle_btn i');
-const menu = document.querySelector('.dropdown_menu');
-const menuLists = document.querySelectorAll('.menu_lists li');
+const toogleBtn = document.querySelector(".toogle_btn");
+const toogleBtnIcon = document.querySelector(".toogle_btn i");
+const menu = document.querySelector(".dropdown_menu");
+const menuLists = document.querySelectorAll(".menu_lists li");
+const navScroll = document.querySelector(".navbar");
 
 /* NAV-FUNCTIONS */
 
 /* ACTIVE LINKS */
 const removeActive = () => {
-  menuLists.forEach(link => {
-    link.classList.remove('active');
+  menuLists.forEach((link) => {
+    link.classList.remove("active");
   });
 };
 
-menuLists.forEach(link => {
-  link.addEventListener('click', () => {
+menuLists.forEach((link) => {
+  link.addEventListener("click", () => {
     removeActive();
-    link.classList.add('active');
+    link.classList.add("active");
   });
 });
 
 toogleBtn.onclick = () => {
-  menu.classList.toggle('open');
+  menu.classList.toggle("open");
 
-  const isOpen = menu.classList.contains('open');
+  const isOpen = menu.classList.contains("open");
 
-  toogleBtnIcon.classList = isOpen ? 'fa fa-times' : 'fa fa-bars';
+  toogleBtnIcon.classList = isOpen ? "fa fa-times" : "fa fa-bars";
 };
 
 const isClose = () => {
-  const isClose = menu.classList.remove('open');
+  const isClose = menu.classList.remove("open");
 
-  toogleBtnIcon.classList = isClose ? 'fa fa-times' : 'fa fa-bars';
+  toogleBtnIcon.classList = isClose ? "fa fa-times" : "fa fa-bars";
 };
 
-document.onclick = e => {
+document.onclick = (e) => {
   if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
     isClose();
   }
 };
-document.onscroll = e => {
+document.onscroll = (e) => {
   if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
     isClose();
   }
 };
+
+/* SCROLL-FUNCTIONS */
+window.addEventListener("scroll", () => {
+  navScroll.classList.toggle("scroll_active", window.scrollY > 350);
+});
+
+// menuListsContainer.forEach((container) => {
+//   window.addEventListener("scroll", () => {
+//     container.classList.toggle("text_primary", window.scrollY > 350);
+//   });
+// });
